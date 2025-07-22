7bff62f841d8ddf41ee7287a413af12c2e614db4
parksyngjun <darkjyuk@naver.com>
2025-07-22 11:51:27 +0900

    .

--- Samsung/2048.py
+++ Samsung/2048.py
@@ -1,5 +1,134 @@
+# 문제: 2048(easy)
+#2048 게임은 4×4 크기의 보드에서 혼자 즐기는 재미있는 게임이다. 이 링크를 누르면 게임을 해볼 수 있다.
+#이 게임에서 한 번의 이동은 보드 위에 있는 전체 블록을 상하좌우 네 방향 중 하나로 이동시키는 것이다. 이때, 같은 값을 갖는 두 블록이 충돌하면 두 블록은 하나로 합쳐지게 된다.
+# 한 번의 이동에서 이미 합쳐진 블록은 또 다른 블록과 다시 합쳐질 수 없다. (실제 게임에서는 이동을 한 번 할 때마다 블록이 추가되지만, 이 문제에서 블록이 추가되는 경우는 없다)
+
+#첫째 줄에 보드의 크기 N (1 ≤ N ≤ 20)이 주어진다. 둘째 줄부터 N개의 줄에는 게임판의 초기 상태가 주어진다. 0은 빈 칸을 나타내며, 이외의 값은 모두 블록을 나타낸다. 블록에 쓰여 있는 수는 2보다 크거나 같고, 1024보다 작거나 같은 2의 제곱꼴이다. 블록은 적어도 하나 주어진다.
+
+
+
+import copy
+import sys
 
 
 
 
-n, m = map(int, input().split())
+
+N = int(input())
+pan = []
+ans = 0
+
+for _ in range(N):
+    pan.append([int(x) for x in sys.stdin.readline().rstrip().split()])
+
+
+
+def left(board):
+    for i in range(N):
+        cursor = 0
+        for j in range(1, N):
+            if board[i][j] != 0: # 0이 아닌 값이
+                tmp = board[i][j]
+                board[i][j] = 0 # 일단 비워질꺼니까 0으로 바꿈
+
+                if board[i][cursor] == 0: # 비어있으면
+                    board[i][cursor] = tmp # 옮긴다
+
+                elif board[i][cursor] == tmp: # 같으면
+                    board[i][cursor] *= 2 # 합친다
+                    cursor += 1
+                else: # 비어있지도 않고 다른 값일때
+                    cursor += 1 # pass
+                    board[i][cursor] = tmp # 바로 옆에 붙임
+
+    return board
+
+def right(board):
+    for i in range(N):
+        cursor = N - 1
+        for j in range(N - 1, -1, -1):
+
+            if board[i][j] != 0:
+                tmp = board[i][j]
+                board[i][j] = 0
+
+                if board[i][cursor] == 0:
+                    board[i][cursor] = tmp
+
+                elif board[i][cursor] == tmp:
+                    board[i][cursor] *= 2
+                    cursor -= 1
+                else:
+                    cursor -= 1
+                    board[i][cursor] = tmp
+    return board
+
+def up(board):
+    for j in range(N):
+        cursor = 0
+        for i in range(N):
+            if board[i][j] != 0:
+                tmp = board[i][j]
+                board[i][j] = 0
+
+                if board[cursor][j] == 0:
+                    board[cursor][j] = tmp
+
+                elif board[cursor][j] == tmp:
+                    board[cursor][j] *= 2
+                    cursor += 1
+
+                else:
+                    cursor += 1
+                    board[cursor][j] = tmp
+    return board
+
+def down(board):
+    for j in range(N):
+        cursor = N - 1
+        for i in range(N - 1, -1, -1):
+            if board[i][j] != 0:
+                tmp = board[i][j]
+                board[i][j] = 0
+
+                if board[cursor][j] == 0:
+                    board[cursor][j] = tmp
+
+                elif board[cursor][j] == tmp:
+                    board[cursor][j] *= 2
+                    cursor -= 1
+
+                else:
+                    cursor -= 1
+                    board[cursor][j] = tmp
+    return board
+# dfs 우선 탐색 알고리즘
+# dfs는 재귀 또는 스택을 사용하여 탐색을 깊게 파고 드는 방식
+# 한 경로를 끝까지 내려가며 탐색하고, 그 다음 경로로 넘어감.
+# 트리 구조나 모든 경로 탐색에 적합.
+
+
+def dfs(n, arr):
+    global ans
+    if n == 5:
+        for i in range(N):
+            for j in range(N):
+                if arr[i][j] > ans:
+                    ans = arr[i][j]
+        return
+
+    # 재귀
+    for i in range(4):
+        copy_arr = copy.deepcopy(arr)
+        if i == 0:
+            dfs(n + 1, left(copy_arr))
+        elif i == 1:
+            dfs(n + 1, right(copy_arr))
+        elif i == 2:
+            dfs(n + 1, up(copy_arr))
+        else:
+            dfs(n + 1, down(copy_arr))
+
+
+dfs(0, pan)
+print(ans)
